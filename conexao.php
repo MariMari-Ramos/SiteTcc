dfae4de2169482e7efdadadd1fdd6eaecc19bbf6
MariMari-Ramos <marianaramos1903@gmail.com>
2025-10-09 16:20:21 -0300

    Pela segunda vez, login e cadastro estão prontos

--- conexao.php
+++ conexao.php
@@ -1,17 +1,16 @@
 
 <?php
-$serverName = "ds-team.database.windows.net,1433"; // endereço do SQL remoto + porta (1433 é padrão)
-$database   = "TCC_banco";          // nome do seu banco
-$username   = "AdminAzure";     // login SQL
-$password   = "Mineirotop123";     // senha SQL
+$servername = "localhost";
+$username = "root"; 
+$password = "usbw"; // ou sua senha do MySQL
+$dbname = "bancotcc";    // senha SQL
+
 
 try {
-    $conn = new PDO("sqlsrv:server=$serverName;Database=$database", $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Conexão realizada com sucesso!";
-} catch (PDOException $e) {
-    die("Conexão falhou: " . $e->getMessage());
-}
+    $conn = new mysqli($servername, $username, $password, $dbname);
+} catch (Exception $e) {
+        die("Falha na conexão: " . $e->getMessage());
+    }
 ?>
 
 
